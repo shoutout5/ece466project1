495b6e177a9aad1ee019e9bbe26ff6ecfdba134d
sgaffer <sgaffer@gmail.com>
2011-10-02 22:55:36 -0300

    Fixed handling of global variables to conform to LLVM, added functionality to handling of functions

--- cetus/LLVMCodeGenPass.java
+++ cetus/LLVMCodeGenPass.java
@@ -42,7 +42,8 @@ public class LLVMCodeGenPass extends cetus.analysis.AnalysisPass
 		}
 
 		iter = new DepthFirstIterator(program);     //reset iterator
-
+		code.println("");
+		
 		//look for procedure declarations
 		while (iter.hasNext()) {
 
@@ -91,34 +92,55 @@ public class LLVMCodeGenPass extends cetus.analysis.AnalysisPass
 			//check for possible initializer
 			Initializer init = dec.getInitializer();
 
-			if(init == null)
-				initVal = "0";
-			else
+			if (init != null)
 			{
 				dump.println("Value Init");
 				initVal = init.toString();
 				initVal = initVal.substring(initVal.indexOf("=")+2,initVal.length());
+				
+				//print global var declaration code
+				code.println("@"+id.getName()+" global i32 " + initVal);
 			}
-
-
-			//print global var declaration code
-			code.println("@"+id.getName()+" common global i32 " + initVal);
+			else
+				code.println("@"+id.getName()+" common global i32 0");		
 		}
 	}    
 
 	private void procedure(Procedure proc)
 	{
-		IDExpression id = proc.getName();
-		List<?> ll = proc.getReturnType();
-		dump.println("Return type is "+ll.get(0));
+		String returnType;
+		StringBuffer argBuff = new StringBuffer("");
+		dump.println("Return type is " + proc.getReturnType());
 		/*	for(int i=0; i<ll.size(); i++)
     	{
     		 System.out.println(ll.get(i)+" num="+i);
 
     	}
 		 */
-		dump.println("The name of this function is "+id.getName());
-		CompoundStatement cs = proc.getBody();
-		dump.println("There are "+cs.countStatements()+" statements in this function.");
+		
+		System.out.println("Parameters: " + proc.getParameters());
+		
+		if (proc.getReturnType().toString().equals("[int]"))
+				returnType = "i32";
+		else returnType = "test";	// TO BE IMPLEMENTED
+		
+		if (!(proc.getParameters().isEmpty() || proc.getParameter(0).toString().equals("void ")))
+		{
+			if (proc.getParameter(0).toString().startsWith("int"))
+				argBuff.append("i32");
+			for (int i = 1; i < proc.getParameters().size(); i++ ) {
+				argBuff.append(", ");
+				if (proc.getParameter(i).toString().startsWith("int)"))
+						argBuff.append("i32");
+				argBuff.append("i32");
+					//{arguments, ", ", proc.getParameter(i).toString()};
+			}
+			//System.out.println("Arguments: " + argBuff);
+		}
+		
+		dump.println("The name of this function is "+proc.getName());
+		dump.println("There are "+proc.getBody().countStatements()+" statements in this function.");
+
+		code.println("define " +returnType + " @" + proc.getName() + "(" + argBuff + ")");
 	}  
 }
