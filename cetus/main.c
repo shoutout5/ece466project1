f7a4479a57eb6be996c34fd4140efee99f529a46
shoutout5 <bubba.bob@gmail.com>
2011-10-11 13:32:48 -0300

    Chris's new main

--- cetus/main.c
+++ cetus/main.c
@@ -1,16 +1,38 @@
 int x, y, z;
 
-int foo1() {
-  return 5;
-}
+int j = 5;
 
-int j;
 
-int foo2() {
-int test;
-  return 5;
-}
 
-int main() {
+int main() 
+{
+  int a=0;
+  int b=0;
+  while(x <= 5)
+  {
+    a = 1 + 2;
+    b = 5;
+  }
+  
+
   return 0;
+
+}
+
+void ifTest(int a)
+{
+    if(a/2 + y < z)
+        z = 4/y + 2*x;
+}
+
+int ifElseTest()
+{
+    if(x == 5)
+        x = 2 + x;
+    else
+        x = y;
+
+    z = 2*x + 7;
+
+    return 3;
 }
